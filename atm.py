29346aca3adb7e857bf51e1f25622e71b525e546
pavanajmadhu <81358710+pavanajmadhu@users.noreply.github.com>
2021-12-30 11:25:34 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,18 @@
+
+class Details:
+    def __init__(self,CardNumber,PinNumber):
+        self.CardNumber=CardNumber
+        self.PinNumber=PinNumber
+    
+    def transaction(amount1):
+        print(f"transaction done for rs.{amount1}")
+        
+    
+    
+    
+cardNumber=input("your card number : ")
+pin=input("your pin : ")
+a=Details(cardNumber,pin)
+amount=input("the amount needed")
+
+a.transaction(amount)
